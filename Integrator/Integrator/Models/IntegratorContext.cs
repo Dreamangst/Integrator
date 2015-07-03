7b524f5dfbe6f50b4d7ea5ec4c8b3d9ae4d87e75
Dreamangst <epm.nicolas@gmail.com>
2015-07-03 15:42:02 -0300

    2nd Push

---
+++ Integrator/Integrator/Models/IntegratorContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Integrator.Models
+{
+    public class IntegratorContext : DbContext
+    {
+        public DbSet<Promo> Promos { get; set; }
+
+    }
+}
---
+++ Integrator/Integrator/Models/Stadium.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Integrator.Models
+{
+    public class Stadium
+    {
+        public Tribunal[] tribunals { get; set; }
+
+        public class Tribunal
+        {
+            public Portal[] portals { get; set; }
+        }
+
+        public class Portal
+        {
+
+        }
+
+
+    }
+}
